1d9a9170d50e63f0e673126af21445e7852510de
Lucas Fischer Azevedo <lazevedo06@gmail.com>
2026-08-17 22:59:11 +0000

    cod_fischer_feito

--- CodificadorFischer.java
+++ CodificadorFischer.java
@@ -1,35 +1,41 @@
 import java.time.LocalDate;
 
-public class CodificadorSimples implements Codificador {
+public class CodificadorFischer implements Codificador {
+
     public String getNome() {
-        return "Codificador Simples";
+        return "Codificador Fischer";
     }
 
     public LocalDate getDataCriacao() {
-        return LocalDate.of(2025, 03, 13);
+        return LocalDate.of(2026, 8, 17);
     }
 
-    public int getNivelSeguranca(){
-        return 1;
+    public int getNivelSeguranca() {
+        return 20;
     }
 
     public String codifica(String str) {
         StringBuilder encoded = new StringBuilder();
 
         for (char c : str.toCharArray()) {
-            encoded.append((char) (c + 1));
+
+            if (c >= 'A' && c <= 'Z') {
+                encoded.append((char) ('Z' - (c - 'A')));
+
+            } else if (c >= 'a' && c <= 'z') {
+                encoded.append((char) ('z' - (c - 'a')));
+
+            } else {
+                encoded.append(c);
+            }
         }
 
         return encoded.toString();
     }
 
     public String decodifica(String str) {
-        StringBuilder encoded = new StringBuilder();
-        
-        for (char c : str.toCharArray()) {
-            encoded.append((char) (c - 1));
-        }
-        
-        return encoded.toString();
+        // O algoritmo Atbash é simétrico:
+        // aplicar o mesmo processo novamente recupera o texto original.
+        return codifica(str);
     }
 }
